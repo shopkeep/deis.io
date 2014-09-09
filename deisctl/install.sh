5b536acc6fbcb198d8ef0246156c921852b6d934
Matt Boersma <matt@opdemand.com>
2014-09-09 14:00:49 -0600

    feat(deisctl): add installer script to pipe into "sudo sh"
    This allows one-liner installs:
    curl -sSL http://deis.io/deisctl/install.sh | sudo sh

---
+++ deisctl/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# determine from whence to download the installer
+PLATFORM=`uname | tr '[:upper:]' '[:lower:]'`
+DEIS_INSTALLER=${DEIS_INSTALLER:-deisctl-0.11.0-$PLATFORM-amd64.run}
+DEIS_BASE_URL=${DEIS_BASE_URL:-https://s3-us-west-2.amazonaws.com/opdemand}
+INSTALLER_URL=$DEIS_BASE_URL/$DEIS_INSTALLER
+
+# download the installer archive to /tmp
+curl -f -o /tmp/$DEIS_INSTALLER $INSTALLER_URL
+
+# run the installer
+sh /tmp/$DEIS_INSTALLER
+
+# clean up after ourselves
+rm -f /tmp/$DEIS_INSTALLER
