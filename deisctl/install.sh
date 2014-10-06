7fa9cb43c8b4fb3f0d2fbc87095b1f43c4d871b8
Matt Boersma <matt@opdemand.com>
2014-10-06 11:48:52 -0600

    fix(install.sh): accept a version argument and anticipate non-root install

--- deisctl/install.sh
+++ deisctl/install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# install current version unless overridden by first command-line argument
+VERSION=${1:-0.13.0-dev}
+
 # try to detect Linux distribution family
 OS=`hostnamectl status 2> /dev/null | sed -n -e 's/^.*Operating\ System: //p' | cut -d " " -f1`
 if [ "$OS" = "CoreOS" ]; then
     mkdir -p /opt/bin
     INSTALLER_OPTS="--target /opt/bin"
+    INSTALL_GLOBAL_UNITS=true
 fi
 
 # catch errors from here on out
@@ -12,7 +16,7 @@ set -e
 
 # determine from whence to download the installer
 PLATFORM=`uname | tr '[:upper:]' '[:lower:]'`
-DEIS_INSTALLER=${DEIS_INSTALLER:-deisctl-0.13.0-dev-$PLATFORM-amd64.run}
+DEIS_INSTALLER=${DEIS_INSTALLER:-deisctl-$VERSION-$PLATFORM-amd64.run}
 DEIS_BASE_URL=${DEIS_BASE_URL:-https://s3-us-west-2.amazonaws.com/opdemand}
 INSTALLER_URL=$DEIS_BASE_URL/$DEIS_INSTALLER
 
@@ -21,6 +25,11 @@ curl -f -o /tmp/$DEIS_INSTALLER $INSTALLER_URL
 
 # run the installer
 sh /tmp/$DEIS_INSTALLER $INSTALLER_OPTS
+
+# install the unit files to /var/lib/deis/units on CoreOS by default
+if [ "$INSTALL_GLOBAL_UNITS" = "true" ]; then
+    /opt/bin/deisctl refresh-units --path=/var/lib/deis/units
+fi
 
 # clean up after ourselves
 rm -f /tmp/$DEIS_INSTALLER
