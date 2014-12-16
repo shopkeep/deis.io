06816b7c6ca17354279f19713252705ed1620113
Matt Boersma <matt@opdemand.com>
2014-12-16 15:46:05 -0700

    chore(install.sh): update version to v1.1.1

--- deisctl/install.sh
+++ deisctl/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install current version unless overridden by first command-line argument
-VERSION=${1:-1.1.0}
+VERSION=${1:-1.1.1}
 
 # try to detect Linux distribution family
 OS=`hostnamectl status 2> /dev/null | sed -n -e 's/^.*Operating\ System: //p' | cut -d " " -f1`
